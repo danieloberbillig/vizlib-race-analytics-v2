4ddb3d66094032ac8aac9d2b80e1a0bc6f26cc77
Daniel Oberbillig <danieloberbillig@gmail.com>
2019-06-12 12:54:44 +1000

    3 chapters

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('ul.tabs').tabs({
+    swipeable: true,
+    responsiveThreshold: 1920
+  });
+
+  $('.collapsible').collapsible();
+});
+
+// $(document).ready(function(){
+//     $('.collapsible').collapsible();
+//   });
